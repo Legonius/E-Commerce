991803a15f8d36168245f7af9ab5f2ea1103c3c1
Legonius <emoinblackzone@gmail.com>
2024-08-20 15:39:06 +0630

    adding pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import About from "./Pages/About";
+import Cart from "./Pages/Cart";
+import Collection from "./Pages/Collection";
+import Contact from "./Pages/Contact";
+import Login from "./Pages/Login";
+import Orders from "./Pages/Orders";
+import PlaceOrder from "./Pages/PlaceOrder";
+import Product from "./Pages/Product";
+import Home from "./Pages/Home";
 
 const App = () => {
-  return <div className=""></div>;
+  return (
+    <div className="">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/place-order" element={<PlaceOrder />} />
+        <Route path="/product" element={<Product />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
---
+++ Frontend/src/Pages/PlaceOrder.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PlaceOrder = () => {
+  return <div></div>;
+};
+
+export default PlaceOrder;
