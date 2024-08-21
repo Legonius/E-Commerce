636ff7b42607b7aea8c33dc79f050e512aee96f7
Legonius <emoinblackzone@gmail.com>
2024-08-21 13:23:54 +0630

    Title compnent created

--- Frontend/src/Components/Title.jsx
+++ Frontend/src/Components/Title.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
-const Title = () => {
-  return <div></div>;
+const Title = ({ text1, text2 }) => {
+  return (
+    <div className="flex items-center gap-2">
+      <h1 className="text-xl text-slate-500 md:text-3xl">
+        {text1}
+        <span className="font-bold text-black ml-1">{text2}</span>
+      </h1>
+      <p className="h-[1.5px] border-none bg-black w-10 md:w-12"></p>
+    </div>
+  );
 };
 
 export default Title;
