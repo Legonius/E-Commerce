e30466ae5372c46b4b25fe1891b67405ccbc1d73
Legonius <emoinblackzone@gmail.com>
2024-08-21 22:22:58 +0630

    OurPolicy componet done with responsiveness

---
+++ Frontend/src/Components/OurPolicy.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { assets } from "../assets/assets";
+
+const OurPolicy = () => {
+  return (
+    <div className="text-slate-400 mb-4 flex flex-col sm:flex-row justify-evenly gap-8 items-center">
+      <div className="flex flex-col justify-center items-center">
+        <img className="h-14 mb-4" src={assets.exchange_icon} />
+        <p className="text-slate-900 ">Easy Exchange Policy</p>
+        <p>We offer hassle free exchange policy</p>
+      </div>
+      <div className="flex flex-col justify-center items-center">
+        <img className="h-14 mb-4" src={assets.quality_icon} />
+        <p className="text-slate-900 ">Easy Exchange Policy</p>
+        <p>We offer hassle free exchange policy</p>
+      </div>
+      <div className="flex flex-col justify-center items-center">
+        <img className="h-14 mb-4" src={assets.support_img} />
+        <p className="text-slate-900 ">Easy Exchange Policy</p>
+        <p>We offer hassle free exchange policy</p>
+      </div>
+    </div>
+  );
+};
+
+export default OurPolicy;
--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "../Components/Hero";
 import LatestCollection from "../Components/LatestCollection";
 import BestSeller from "../Components/BestSeller";
+import OurPolicy from "../Components/OurPolicy";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Hero />
       <LatestCollection />
       <BestSeller />
+      <OurPolicy />
     </div>
   );
 };
