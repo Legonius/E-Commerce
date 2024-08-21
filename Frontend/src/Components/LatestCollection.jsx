28928cdd287775a83dcd7c0abb9ad16ae78d4191
Legonius <emoinblackzone@gmail.com>
2024-08-21 15:14:56 +0630

    start Latest Collection components

---
+++ Frontend/src/Components/LatestCollection.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Title from "./Title";
+
+const LatestCollection = () => {
+  return (
+    <div>
+      <Title text1={"LATEST"} text2={"COLLECTIONS"} />
+    </div>
+  );
+};
+
+export default LatestCollection;
--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "../Components/Hero";
+import LatestCollection from "../Components/LatestCollection";
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <LatestCollection />
     </div>
   );
 };
