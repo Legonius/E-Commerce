7bf25994067ec7b267fc2ea55e909b49bd9e48e4
Legonius <emoinblackzone@gmail.com>
2024-08-21 15:04:36 +0630

    Hero components making responsive

--- Frontend/src/Components/Hero.jsx
+++ Frontend/src/Components/Hero.jsx
@@ -4,17 +4,20 @@ import { assets } from "../assets/assets";
 
 const Hero = () => {
   return (
-    <div className="flex flex-col sm:flex-row border-2 p-0">
-      <div className="w-full flex flex-col justify-center items-center">
+    <div className=" flex  flex-col sm:flex-row border-2 p-0">
+      {/* {hero left side} */}
+      <div className="w-full sm:w-1/2 flex flex-col justify-center items-center py-10 sm:py-0 flex-shrink-0">
         <div className="flex flex-col gap-3 text-slate-500 ">
           <div className="flex gap-2 items-center">
-            <p className="h-[2px] w-12 bg-slate-500"></p>
+            <p className="h-[2px] w-9 sm:w-12 bg-slate-500"></p>
             <p className=" font-bold">OUR BESTSELLERS</p>
           </div>
-          <h2 className="crimson-text-regular text-5xl">Latest Arrivals</h2>
+          <h2 className="crimson-text-regular text-5xl leading-relaxed">
+            Latest Arrivals
+          </h2>
           <div className="flex gap-2 items-center">
             <p className=" font-bold">SHOP NOW</p>
-            <p className="h-[1.5px] w-12 bg-slate-500"></p>
+            <p className="h-[1.5px] w-9 sm:w-12 bg-slate-500"></p>
           </div>
         </div>
       </div>
