e5f985896cd667d51e2f3a49cf17ca76900c330f
Legonius <emoinblackzone@gmail.com>
2024-09-16 17:28:14 +0630

    models setup done

---
+++ Backend/models/productModel.js
@@ -0,0 +1,16 @@
+import { Schema, model } from "mongoose";
+import mongoose from "mongoose";
+
+const productSchema = new Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: String, required: true },
+  image: { type: Array, required: true },
+  category: { type: String, required: true },
+  subCategory: { type: String, required: true },
+  sizes: { type: Array, required: true },
+  bestseller: { type: Boolean },
+  date: { type: Number, required: true },
+});
+const productModel = mongoose.models.product || model("product", productSchema);
+export default productModel;
