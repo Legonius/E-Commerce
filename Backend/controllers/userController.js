704d387422c057ee0234ea4a4a930f598352fa80
Legonius <emoinblackzone@gmail.com>
2024-09-16 21:09:49 +0630

    longin controller done

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -12,38 +12,73 @@ const makeToken = (id) => {
 };
 
 // controller for user login
-const loginUser = async (req, res) => {};
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid Credentials" });
+    }
+
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User doesn't exist!" });
+    }
+    const checkPassword = await bcryptjs.compare(password, user.password);
+    if (!checkPassword) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Password is incorrect" });
+    }
+    const token = makeToken(user._id);
+    res.status(202).json({ success: true, message: token });
+  } catch (error) {
+    console.log("err:", error);
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
 
 // controller for user register
 const registerUser = async (req, res) => {
-  const { name, email, password } = req.body;
+  try {
+    const { name, email, password } = req.body;
 
-  if (!validator.isEmail(email)) {
-    return res.json({ success: false, message: "Invalid Email" });
-  }
-  const exist = await userModel.findOne({
-    email: email,
-  });
-  if (exist) {
-    return res.json({ success: false, message: "User already exists." });
-  }
-  if (password.length < 8) {
-    return res.json({
-      success: false,
-      message: "Please enter Strong Password",
+    if (!validator.isEmail(email)) {
+      return res.status(400).json({ success: false, message: "Invalid Email" });
+    }
+    const exist = await userModel.findOne({
+      email: email,
     });
-  }
-  const genSalt = await bcryptjs.genSalt();
-  const hashPassword = await bcryptjs.hash(password, genSalt);
+    if (exist) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User already exists." });
+    }
+    if (password.length < 8) {
+      return res.status(400).json({
+        success: false,
+        message: "Please enter Strong Password",
+      });
+    }
+    const salt = await bcryptjs.genSalt(11);
+    const hashPassword = await bcryptjs.hash(password, salt);
 
-  const register = new userModel({
-    name,
-    email,
-    password: hashPassword,
-  });
-  const newUser = await register.save();
-  const token = makeToken(newUser._id);
-  res.json({ success: true, message: token });
+    const register = new userModel({
+      name,
+      email,
+      password: hashPassword,
+    });
+    const newUser = await register.save();
+    const token = makeToken(newUser._id);
+    res.status(201).json({ success: true, message: token });
+  } catch (error) {
+    console.log("err:", error);
+    res.status(500).json({ success: false, message: error.message });
+  }
 };
 
 // controller for admin login
